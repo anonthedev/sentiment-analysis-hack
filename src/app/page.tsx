be9215c203307d90d9206fd765d886113e32dc89
anonthedev <opss59332@gmail.com>
2025-01-19 11:48:21 +0530

    fixing prod errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-// @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+
 import landingHTML from '../components/Landing.html';
 
 export default function Home() {
